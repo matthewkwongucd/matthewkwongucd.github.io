3a41af0aab70f495303795e3525df7807bc0f998
Matthew Kwong <mk1006@scc2102-23.ucdavis.edu>
2020-03-03 10:35:27 -0800

    add exercises

---
+++ des157-wi20/exercises/tabs-start/script.js
@@ -0,0 +1,33 @@
+(function(){
+
+  "use strict";
+
+  var tabs = document.querySelectorAll('#tabs > ul > li > a');
+
+  for (var i = 0; i < tabs.length; i++){
+    tabs[i].addEventListener('click', selectTab);
+  }
+
+  function selectTab(event){
+
+    for(var i= 0; i < tabs.length; i++) {
+      tabs[i].removeAttribute('class');
+    }
+    event.target.className = 'active';
+
+    var thisTab = event.target.getAttribute('href');
+    var thisContent = document.querySelector(thisTab);
+
+    var oldTabContent = document.querySelector('.visible');
+    oldTabContent.className = 'visuallyhidden';
+
+    oldTabContent.addEventListener('transitionend', function(){
+      oldTabContent.className  = 'hidden';
+
+      thisContent.className = 'visible visuallyhidden';
+      setTimeout(function(){
+        thisContent.classList.remove('visuallyhidden');
+      }, 20);
+    }, {capture: false, once: true, passive: false});
+  }
+}());
