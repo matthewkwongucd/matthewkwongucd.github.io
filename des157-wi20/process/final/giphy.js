df78c9d53983438ff3b4356b9302e3f1632a553b
kn <kn@kns-MacBook-Pro.local>
2020-03-08 13:54:43 -0700

    giphy random image works

--- des157-wi20/process/final/giphy.js
+++ des157-wi20/process/final/giphy.js
@@ -1,9 +1,10 @@
 
 const apiKey = "ZziyfZIZplQ1ryLkR4yGcSM93JTcnpZJ";
-const q = "bullying";
-const type =  "random";
+const q = ["bullying", "cyberbully", "bully", "cyberbullying", "bullied", "cyberbullied"];
 
-const path = `https://api.giphy.com/v1/gifs/search?&api_key=${apiKey}&q=${q}`;
+
+
+const path = `https://api.giphy.com/v1/gifs/search?&api_key=${apiKey}&q=${q[Math.floor(Math.random()*q.length)]}`;
 
 // makes connection to server
 // const promise = fetch(path); //Promise 1
@@ -26,24 +27,25 @@ fetch(path).then(function(response) {
 }).then(function(json){
 
 	// console.log(json.data); //array of gif results
-	console.log(json.data); //array of gif results
+	// console.log(json.data); //array of gif results
 
-	console.log(json.data[Math.floor(Math.random()*json.data.length)].images.fixed_width.url); //gets RANDOM image
+	// console.log(json.data[Math.floor(Math.random()*json.data.length)].images.fixed_width.url); //gets RANDOM image
 
 	const background = document.getElementById('background');
 	let resultsHTML = '';
 
 
 	// const obj = json.parse(json);
-	json.data.forEach(function(obj){
+	// json.data.forEach(function(obj){
+		// console.log(obj.images.fixed_width.url); //img url
 
-
-		const url = obj.images.fixed_width.url; //img url
+		const url = json.data[Math.floor(Math.random()*json.data.length)].images.fixed_width.url; //img url
+		// list.push(url);
 
 		resultsHTML += `<img src = "${url}"
 		 								width = "100%"
-										alt = "${obj.title}">` //append to background
-	});
+										alt = "${json.data.title}">` //append to background
+
 
 	background.innerHTML = resultsHTML;
 
